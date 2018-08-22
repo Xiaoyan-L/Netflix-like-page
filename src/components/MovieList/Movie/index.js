26b495e1e2be295a5831f9823072c703f29fd972
Xiaoyan Liao <running923@gmail.com>
2018-08-22 11:55:27 -0700

    feature: remove movie id

--- src/components/MovieList/Movie/index.js
+++ src/components/MovieList/Movie/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 const Movie = ({movie, btnValue, hanleClick}) => (
   <div className="Movie">
     <img className="Movie_poster" src={movie.img} alt={movie.title} />
-    <p className="Movie_p"> ID: {movie.id} </p>
     <p className="Movie_p"> {movie.title} </p>
     <button className="Movie_btn" onClick={hanleClick}>
       {btnValue}
